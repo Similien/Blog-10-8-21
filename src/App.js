88eb3fbe5567287d4f92323ad188d0252e77f572
Similien <ssimilien86@gmail.com>
2021-10-08 04:47:34 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import React from "react";
+
+function app() {
+  return (
+    <div className="App">
+      <h1>Blog</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+export default function App() {
+  return app();
+}
